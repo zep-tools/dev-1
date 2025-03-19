6917606a39b9716e21e28743feaf8b28a6edc332
unhyif <jihyun.kim@zep.us>
2025-03-20 01:56:53 +0900

    슬랙 봇의 명령어 처리 개선: app.processEvent 대신 app.start 메서드를 사용하여 요청 처리 방식을 변경하고, 응답 로직을 간소화함

--- api/slack.js
+++ api/slack.js
@@ -60,7 +60,7 @@ app.command("/dev_프론트_퀴즈", async ({ ack, client, command }) => {
 
 export default async (req, res) => {
   try {
-    await app.processEvent(req);
+    await app.start();
     res.status(200).send();
   } catch (error) {
     console.error(error);
