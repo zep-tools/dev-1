7e809fa3b80c57cbbf92e17d5cf086a9d7b3c6ef
unhyif <ang21icjh@gmail.com>
2025-03-19 23:44:06 +0900

    슬랙 봇의 소켓 모드 비활성화: socketMode 설정을 제거하여 기본 모드로 변경

--- api/slack.js
+++ api/slack.js
@@ -5,7 +5,6 @@ const app = new SlackBolt.App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   appToken: process.env.SLACK_APP_TOKEN,
-  socketMode: true,
 });
 
 const octokit = new Octokit({
