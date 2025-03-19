0756f32d72c256eaa1e293e7396a937b2d2796d9
unhyif <ang21icjh@gmail.com>
2025-03-19 23:29:17 +0900

    슬랙 봇 기능 추가 및 패키지 설정: .gitignore, package.json, pnpm-lock.yaml 파일 생성 및 슬랙 API와 Octokit을 사용한 기본 슬랙 봇 구현

---
+++ api/slack.js
@@ -0,0 +1,68 @@
+import { Octokit } from "@octokit/rest";
+import SlackBolt from "@slack/bolt";
+
+const app = new SlackBolt.App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
+});
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
+
+const BRANCHES = [
+  "publish/dev-1",
+  "publish/dev-2",
+  "publish/dev-3",
+  "publish/dev-4",
+  "publish/dev-5",
+  "publish/dev-6",
+  "publish/dev-7",
+  "publish/dev-8",
+  "publish/stage",
+];
+
+export default async (req, res) => {
+  try {
+    await app.start();
+
+    app.command("/dev_프론트_퀴즈", async ({ ack, client, command }) => {
+      await ack();
+
+      const thread = await client.chat.postMessage({
+        channel: command.channel_id,
+        text: "🔎 dev_프론트_퀴즈",
+      });
+      const threadTs = thread.ts;
+
+      let message = "";
+      for (const branch of BRANCHES) {
+        const { data: commit } = await octokit.repos.getCommit({
+          owner: "zep-us",
+          repo: "zep-quiz-client",
+          ref: branch,
+        });
+
+        message += `• \`${branch}\`\n`;
+        message += `${new Date(commit.commit.author.date).toLocaleString(
+          "ko-KR",
+          { timeZone: "Asia/Seoul" }
+        )}\n`;
+        message += `${commit.commit.message}\n\n`;
+      }
+
+      await client.chat.postMessage({
+        channel: command.channel_id,
+        text: message.trim(),
+        thread_ts: threadTs,
+      });
+    });
+
+    res.status(200).send("Slack bot is running!");
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error starting Slack bot");
+  }
+};
